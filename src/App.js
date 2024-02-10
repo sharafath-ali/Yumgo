cbd27f8c5c99a9881784b87591aa0a0a6205ca84
sharafath ali <sharafathaliofficial@gmail.com>
2024-02-10 22:41:45 +0530

    'header parts made responsive'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className='k'>
-      <Header/>
-      <div>
-
-      </div>
+    <div className="">
+      <Header />
+      <div></div>
     </div>
   );
 }
